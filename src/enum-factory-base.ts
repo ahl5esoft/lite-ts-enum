a70fa24272c5d22bae0d69ef3ad430533c0b598c
linyimin <ahl5esoft@gmail.com>
2023-03-21 10:48:57 +0800

    docs: v1.6.0

--- src/enum-factory-base.ts
+++ src/enum-factory-base.ts
@@ -2,5 +2,7 @@ import { EnumItem } from './enum-item';
 import { Enum } from './enum';
 
 export abstract class EnumFactoryBase {
+    public static ctor = 'EnumFactoryBase';
+
     public abstract build<T extends EnumItem>(nameOrCtor: string | (new () => T), areaNo?: number): Enum<T>;
 }
