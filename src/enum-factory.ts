e520045bb616da79641855bd661c9329b725f27a
连建伟 <3478289084@qq.com>
2023-03-31 11:22:09 +0800

    feat: 修改EnumFactory reduceFunc函数

--- src/enum-factory.ts
+++ src/enum-factory.ts
@@ -6,17 +6,22 @@ import { LoadEnumHandlerBase } from './load-handler-base';
 export class EnumFactory extends EnumFactoryBase {
     public constructor(
         private m_LoadHandler: LoadEnumHandlerBase,
-        private m_ReduceFunc: { [key: string]: (memo: any, item: any) => any; },
+        private m_ReduceFunc: {
+            [enumName: string]: {
+                [key: string]: (memo: any, item: any) => any;
+            };
+        },
     ) {
         super();
     }
 
     public build<T extends EnumItem>(nameOrCtor: string | (new () => T), areaNo = 0) {
+        const enumName = typeof nameOrCtor == 'string' ? nameOrCtor : nameOrCtor.name;
         return new Enum(
-            typeof nameOrCtor == 'string' ? nameOrCtor : nameOrCtor.name,
+            enumName,
             areaNo,
             this.m_LoadHandler,
-            this.m_ReduceFunc,
+            this.m_ReduceFunc[enumName] ?? {},
         );
     }
 }
