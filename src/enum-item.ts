b722d29fabcfebdf0321e86ccde84571b670e7bd
连建伟 <3478289084@qq.com>
2023-04-19 11:25:23 +0800

    feat: EnumItem删除构造函数,添加static create方法

--- src/enum-item.ts
+++ src/enum-item.ts
@@ -1,18 +1,19 @@
 export class EnumItem {
+    private m_Name: string;
+
     public value: number;
 
     public key?: string;
 
     public text?: string;
 
-    public constructor(
-        private m_Name: string,
-        entry: EnumItem,
-    ) {
-        Object.assign(this, entry);
-    }
-
     public getEncodingKey(attr: string) {
         return [this.m_Name, this.value, attr].join('-');
+    }
+
+    public static create(name: string, entry: EnumItem) {
+        const item = Object.assign(new EnumItem(), entry);
+        item.m_Name = name;
+        return item;
     }
 }
