8e6880ff1d2d8ec11ed5daeeab1cd070bb42fd40
林学葵 <linxuekui@fengling.cn>
2023-04-19 15:42:37 +0800

    feat：枚举工厂扩展#16295

--- src/enum-factory-base.ts
+++ src/enum-factory-base.ts
@@ -1,8 +1,8 @@
-import { EnumItem } from './enum-item';
 import { Enum } from './enum';
-
+import { EnumBuildOption } from './enum-build-option';
+import { EnumItem } from './enum-item';
 export abstract class EnumFactoryBase {
     public static ctor = 'EnumFactoryBase';
 
-    public abstract build<T extends EnumItem>(nameOrCtor: string | (new () => T), areaNo?: number): Enum<T>;
+    public abstract build<T extends EnumItem>(opt: EnumBuildOption<T>): Enum<T>;
 }
--- src/load-handler-context.ts
+++ src/load-handler-context.ts
@@ -1,7 +1,8 @@
 import { Enum } from './enum';
 
 export type LoadEnumHandlerContext = Partial<{
+    app: string;
+    areaNo: number;
     enum: Enum<any>;
     res: { [no: number]: any; };
-    areaNo: number;
 }>;
